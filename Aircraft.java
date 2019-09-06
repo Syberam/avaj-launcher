ca24f6127cf9c1feb0553c886a4c260fa31e0261
Sylvain BONNEFON <sylvainbonnefon@hotmail.com>
2019-09-06 09:56:30 +0200

    empty files

---
+++ Aircraft.java
@@ -0,0 +1,11 @@
+class Aircraft {
+	//attributs
+	//# id : long
+	//# name : String
+	//# coordinates : Coordinates
+	//-idCounter : long
+
+	//methods
+	//#Aircraft(name: String, coordinates: Coordinates)
+	//-nextId() : long
+}
