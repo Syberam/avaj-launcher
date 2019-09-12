fc448c11b6d9314237c6e8d3b17d679bea464235
Sylvain BONNEFON <sbonnefo@student.42.fr>
2019-09-12 14:33:00 +0200

    corrected AircraftFactory varaible classe syntax mistake

--- AircraftFactory.java
+++ AircraftFactory.java
@@ -1,14 +1,14 @@
 class AircraftFactory {
     public static Flyable newAircraft(String type, String name,
     int longitude, int latitude, int height) {
-		coordonates = new Coordonates(longitude, latitude, height);
+		Coordinates coordinates = new Coordinates(longitude, latitude, height);
 
 		if (type.compareTo("Helicopter") == 0)
-			return new Helicopter(name, coordonates);
+			return new Helicopter(name, coordinates);
 		else if (type.compareTo("Baloon") == 0)
-			return new Ballon(name, coordonates);
+			return new Baloon(name, coordinates);
 		else if (type.compareTo("JetPlane") == 0)
-			return new JetPlane(name, coordonates);
+			return new JetPlane(name, coordinates);
 		return null;
     }
 }
