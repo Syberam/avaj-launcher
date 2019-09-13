01d881f08a678ad000397740603c6732e4556dca
Sylvain BONNEFON <sbonnefo@student.42.fr>
2019-09-13 10:46:21 +0200

    add usage and ignore ressource.txt file

--- com/launcher/airport/Simulator.java
+++ com/launcher/airport/Simulator.java
@@ -9,7 +9,7 @@ public class Simulator {
 		WeatherTower wt = new WeatherTower();
 		int rounds = 0;
 	
-		String fileName = "scenario.txt";
+		String fileName = "ressources/scenario.txt";
 		if (args.length > 0) {
 			fileName = args[0];
 		}
@@ -52,14 +52,20 @@ public class Simulator {
 			System.out.println(
 				"Unable to open file '" +
 				fileName + "'");
+			System.out.println(usage());
+			
 		}
 		catch(IOException ex) {
 			System.out.println(
 				"Error reading file '" 
 				+ fileName + "'");
+			System.out.println(usage());
 		}
 		catch(NumberFormatException ex) {
-				System.out.println("Invalid number of rounds: " + ex.getMessage());
+				System.out.println(
+					"Invalid number of rounds: "
+					+ ex.getMessage());
+				System.out.println(usage());
 			}
 
 		for (int i = 0; i < rounds; i++){
@@ -68,5 +74,28 @@ public class Simulator {
 				break;
 			}
 		}
+	}
+
+	static String usage() {
+		return String.join(
+			System.getProperty("line.separator")
+			, "Input file must to be format as folllow:"
+			, System.getProperty("line.separator")
+			, "R"
+			, "TYPE_1 NAME_1 LONGITUDE_1 LATITUDE_1 HEIGHT_1"
+			, "TYPE_2 NAME_2 LONGITUDE_2 LATITUDE_2 HEIGHT_2"
+			, "..."
+			, "TYPE_n-1 NAME_n-1 LONGITUDE_n-1 LATITUDE_n-1 HEIGHT_n-1"
+			, "TYPE_n NAME_n LONGITUDE_n LATITUDE_n HEIGHT_n"
+			, System.getProperty("line.separator")
+			, "Where :"
+			, "R\t\t\t: for rounds, the number of time the conditions will change"
+			, "TYPE_n (String)\t\t: the aircraft nºn type"
+			, "NAME_n (String)\t\t: the aircraft nºn name"
+			, "LONGITUDE_n (int)\t: aircraft nºn longitude position"
+			, "LATITUDE_n (int)\t: aircraft nºn latitude position"
+			, "HEIGHT_n (int)\t\t: aircraft nºn height position"
+			, "n\t\t\t: the number of aircrafts"
+		);
 	}
 }
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 find . -name "*.java" > sources.txt
 javac -sourcepath . @sources.txt
-java com.launcher.airport.Simulator ressources/scenario.txt
-#java com.launcher.airport.Simulator test
+java com.launcher.airport.Simulator
 rm -Rf ./**/*.class
